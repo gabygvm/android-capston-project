c93fea143750c4e81695f8b272c7f9317ebfbb01
gabygvm <gabygvm.87@gmail.com>
2014-10-23 00:06:51 -0500

    Agregado image View para la actividad principal de los pacientes.

--- ClientWithOauth2/src/org/magnum/symptom/client/PatientActivity.java
+++ ClientWithOauth2/src/org/magnum/symptom/client/PatientActivity.java
@@ -4,22 +4,30 @@ import org.magnum.videoup.client.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class PatientActivity extends Activity {
 
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+	private ImageView _imageView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_patient);
 		
-		Button launchCheckIn = (Button) findViewById(R.id.checkInButton); 
-		launchCheckIn.setOnClickListener(new OnClickListener() {
+		_imageView = (ImageView) findViewById(R.id.imageView1);
+		
+		Button checkInLaunch = (Button)findViewById(R.id.checkInButton); 
+		checkInLaunch.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {		
@@ -27,7 +35,51 @@ public class PatientActivity extends Activity {
 				startActivity(myIntent);
 			}
 		});
+		
+		
+		Button editImageButton = (Button)findViewById(R.id.changePicButton);
+		editImageButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				dispatchTakePictureIntent();
+			}
+		});
 	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+	        Bundle extras = data.getExtras();
+	        Bitmap imageBitmap = (Bitmap) extras.get("data");
+	        
+	        _imageView.setImageBitmap(imageBitmap);
+	    }
+	}
+	private void dispatchTakePictureIntent() 
+	{
+	    Intent intentTakePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    
+	    if (intentTakePic.resolveActivity(getPackageManager()) != null)
+	    {
+	        startActivityForResult(intentTakePic, REQUEST_IMAGE_CAPTURE);
+	    }
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
